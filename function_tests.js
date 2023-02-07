ee0a38eaaca6b2f8ba271df5e35ee5bf44ee1e9d
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2023-02-07 16:50:06 -0500

    feat: add best of five (#8)

--- function_tests.js
+++ function_tests.js
@@ -43,48 +43,101 @@
 
 // console.log(determineWinner("paper", "rock"));
 
-const readline = require("readline-sync");
-const VALID_CHOICES = ["rock", "paper", "scissors"];
+// const readline = require("readline-sync");
+// const VALID_CHOICES = ["rock", "paper", "scissors"];
 
-const userSelection = getUserSelection();
-const userChoice = validateUserChoice(userSelection);
+// const userSelection = getUserSelection();
+// const userChoice = validateUserChoice(userSelection);
 
-function getUserSelection() {
-  printMessage(
-    `Please choose one of the following options: ${VALID_CHOICES.join(", ")}.`
-  );
-  let choice = readline.question().toLowerCase();
+// function getUserSelection() {
+//   printMessage(
+//     `Please choose one of the following options: ${VALID_CHOICES.join(", ")}.`
+//   );
+//   let choice = readline.question().toLowerCase();
 
-  while (
-    !VALID_CHOICES.includes(choice) &&
-    choice !== "r" &&
-    choice !== "p" &&
-    choice !== "s"
-  ) {
-    printMessage("Oops! That's not a valid choice. Please choose again.");
-    choice = readline.question();
-  }
+//   while (
+//     !VALID_CHOICES.includes(choice) &&
+//     choice !== "r" &&
+//     choice !== "p" &&
+//     choice !== "s"
+//   ) {
+//     printMessage("Oops! That's not a valid choice. Please choose again.");
+//     choice = readline.question();
+//   }
 
-  return choice[0];
+//   return choice[0];
+// }
+
+// //console.log(getUserSelection());
+
+// function validateUserChoice(userSelection) {
+//   if (userSelection[0] === "r") {
+//     return "rock";
+//   } else if (userSelection[0] === "p") {
+//     return "paper";
+//   } else {
+//     return "scissors";
+//   }
+// }
+
+// console.log(userChoice);
+
+// function printMessage(message) {
+//   console.log(`=> ${message}`);
+// }
+
+//BEST-OF-FIVE Feature:
+
+const USER_WINS_ROUND = "You win this round!";
+const COMP_WINS_ROUND = "I win this round!";
+const TIE_MSG = "It's a tie!";
+const USER_WINS_GAME = "You win the game!";
+const COMP_WINS_GAME = "I win the game!";
+
+let userScore = 0;
+let compScore = 0;
+let tie = 0;
+
+//assumed functions
+// const userSelection = getUserSelection();
+// const userChoice = validateUserChoice(userSelection);
+// const computerChoice = determineComputerChoice();
+// displayChoices(userChoice, computerChoice);
+
+function playRound(userChoice, computerChoice) {
+  const gameResults = {
+    rock: {
+      rock: TIE_MSG,
+      paper: COMP_WINS_ROUND,
+      scissors: USER_WINS_ROUND,
+    },
+    paper: {
+      rock: USER_WINS_ROUND,
+      paper: TIE_MSG,
+      scissors: COMP_WINS_ROUND,
+    },
+    scissors: {
+      rock: COMP_WINS_ROUND,
+      paper: USER_WINS_ROUND,
+      scissors: TIE_MSG,
+    },
+  };
+  return gameResults[userChoice][computerChoice];
 }
 
-//console.log(getUserSelection());
-
-function validateUserChoice(userSelection) {
-  if (userSelection[0] === "r") {
-    return "rock";
-  } else if (userSelection[0] === "p") {
-    return "paper";
+function scoreTracker(winner) {
+  if (winner === USER_WINS_ROUND) {
+    userScore++;
+  } else if (winner === COMP_WINS_ROUND) {
+    compScore++;
   } else {
-    return "scissors";
+    tie++;
   }
-}
 
-console.log(userChoice);
-
-//improve input validation features
-//add feature: best of 5
-
-function printMessage(message) {
-  console.log(`=> ${message}`);
+  if (userScore === 3) {
+    console.log(USER_WINS_GAME);
+  }
+  if (compScore === 3) {
+    console.log(COMP_WINS_GAME);
+  }
 }
--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,16 +1,33 @@
 const readline = require("readline-sync");
 const VALID_CHOICES = ["rock", "paper", "scissors"];
 
+const USER_WINS_ROUND = "You win this round!";
+const COMP_WINS_ROUND = "I win this round!";
+const TIE_MSG = "It's a tie!";
+const USER_WINS_GAME = "You win the game!";
+const COMP_WINS_GAME = "I win the game!";
+
+let userScore = 0;
+let compScore = 0;
+let tie = 0;
+
 //main function
 function startRPSGame() {
   greeting();
   while (true) {
-    const userSelection = getUserSelection();
-    const userChoice = validateUserChoice(userSelection);
-    const computerChoice = determineComputerChoice();
-    displayChoices(userChoice, computerChoice);
-    const winner = determineWinner(userChoice, computerChoice);
-    displayWinner(winner);
+    for (let i = 0; i < 100; i++) {
+      const userSelection = getUserSelection();
+      const userChoice = validateUserChoice(userSelection);
+      const computerChoice = determineComputerChoice();
+      displayChoices(userChoice, computerChoice);
+      const winner = playRound(userChoice, computerChoice);
+      displayWinner(winner);
+      scoreTracker(winner);
+
+      if (userScore === 3 || compScore === 3) {
+        break;
+      }
+    }
     const anotherGame = keepPlaying();
     if (anotherGame[0] !== "y") break;
     console.clear();
@@ -68,25 +85,22 @@ function displayChoices(userChoice, computerChoice) {
   printMessage(`You chose ${userChoice}, I chose ${computerChoice}.`);
 }
 
-function determineWinner(userChoice, computerChoice) {
-  const userWinsMsg = "You win!";
-  const compWinsMsg = "I win!";
-  const tieMsg = "It's a tie!";
+function playRound(userChoice, computerChoice) {
   const gameResults = {
     rock: {
-      rock: tieMsg,
-      paper: compWinsMsg,
-      scissors: userWinsMsg,
+      rock: TIE_MSG,
+      paper: COMP_WINS_ROUND,
+      scissors: USER_WINS_ROUND,
     },
     paper: {
-      rock: userWinsMsg,
-      paper: tieMsg,
-      scissors: compWinsMsg,
+      rock: USER_WINS_ROUND,
+      paper: TIE_MSG,
+      scissors: COMP_WINS_ROUND,
     },
     scissors: {
-      rock: compWinsMsg,
-      paper: userWinsMsg,
-      scissors: tieMsg,
+      rock: COMP_WINS_ROUND,
+      paper: USER_WINS_ROUND,
+      scissors: TIE_MSG,
     },
   };
   return gameResults[userChoice][computerChoice];
@@ -96,15 +110,40 @@ function displayWinner(winner) {
   printMessage(winner);
 }
 
+function scoreTracker(winner) {
+  if (winner === USER_WINS_ROUND) {
+    userScore++;
+  } else if (winner === COMP_WINS_ROUND) {
+    compScore++;
+  } else {
+    tie++;
+  }
+
+  if (userScore === 3) {
+    printMessage(USER_WINS_GAME);
+  }
+  if (compScore === 3) {
+    printMessage(COMP_WINS_GAME);
+  }
+}
+
 function keepPlaying() {
-  printMessage("Do you want to play again? Choose 'y' or 'n'.");
+  let validYesOrNo = ["yes", "no"];
+
+  printMessage(
+    `Do you want to play again? Choose ${validYesOrNo.join(" or ")}.`
+  );
   anotherGame = readline.question().toLowerCase();
 
-  while (anotherGame[0] !== "n" && anotherGame[0] !== "y") {
-    printMessage("Please enter 'y or 'n').");
+  while (
+    !validYesOrNo.includes(anotherGame) &&
+    anotherGame !== "n" &&
+    anotherGame !== "y"
+  ) {
+    printMessage("Please choose: 'yes' or 'no'.");
     anotherGame = readline.question().toLocaleLowerCase();
   }
-  return anotherGame.toLocaleLowerCase();
+  return anotherGame;
 }
 
 function printMessage(message) {
