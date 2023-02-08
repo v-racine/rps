8ad7fb35397c3e5e1a97b1907214c383b928efd1
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2023-02-08 16:59:59 -0500

    refactor scoreTracker (#16)

--- function_tests.js
+++ function_tests.js
@@ -16,60 +16,6 @@
 //   }
 // }
 
-//fix function:
-// function determineWinner(userChoice, computerChoice) {
-//   const userWinsMsg = "You win!";
-//   const compWinsMsg = "I win!";
-//   const tieMsg = "It's a tie!";
-//   const gameResults = {
-//     rock: {
-//       rock: tieMsg,
-//       paper: compWinsMsg,
-//       scissors: userWinsMsg,
-//     },
-//     paper: {
-//       rock: userWinsMsg,
-//       paper: tieMsg,
-//       scissors: compWinsMsg,
-//     },
-//     scissors: {
-//       rock: compWinsMsg,
-//       paper: userWinsMsg,
-//       scissors: tieMsg,
-//     },
-//   };
-//   return gameResults[userChoice][computerChoice];
-// }
-
-// console.log(determineWinner("paper", "rock"));
-
-// const readline = require("readline-sync");
-// const VALID_CHOICES = ["rock", "paper", "scissors"];
-
-// const userSelection = getUserSelection();
-// const userChoice = validateUserChoice(userSelection);
-
-// function getUserSelection() {
-//   printMessage(
-//     `Please choose one of the following options: ${VALID_CHOICES.join(", ")}.`
-//   );
-//   let choice = readline.question().toLowerCase();
-
-//   while (
-//     !VALID_CHOICES.includes(choice) &&
-//     choice !== "r" &&
-//     choice !== "p" &&
-//     choice !== "s"
-//   ) {
-//     printMessage("Oops! That's not a valid choice. Please choose again.");
-//     choice = readline.question();
-//   }
-
-//   return choice[0];
-// }
-
-// //console.log(getUserSelection());
-
 // function validateUserChoice(userSelection) {
 //   if (userSelection[0] === "r") {
 //     return "rock";
@@ -81,10 +27,6 @@
 // }
 
 // console.log(userChoice);
-
-// function printMessage(message) {
-//   console.log(`=> ${message}`);
-// }
 
 //BEST-OF-FIVE Feature:
 
